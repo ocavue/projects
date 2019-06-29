ff3435fbe67aa74b8f2d5e4e31a60d61e389e635
circleci <ci@circleci.com>
2019-06-29 21:16:06 +0000

    update

---
+++ rino/precache-manifest.28618add3245357a1a1e8bee50473007.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "ebcd0feaf33077519f9e",
+    "url": "/js/chunk-vendors.4eca8720.js"
+  },
+  {
+    "revision": "04f7566d218bdfade72f",
+    "url": "/js/chunk-e5e16eb4.fbbb2d78.js"
+  },
+  {
+    "revision": "313042ac7d267db2a47b",
+    "url": "/js/app.3398c609.js"
+  },
+  {
+    "revision": "8c4e3e76d9e4a28da4868ca87a271e68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebcd0feaf33077519f9e",
+    "url": "/css/chunk-vendors.cb82569e.css"
+  },
+  {
+    "revision": "313042ac7d267db2a47b",
+    "url": "/css/app.0a1e99fa.css"
+  }
+];
